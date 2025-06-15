659de80272974685bfd06b13f178efaabcf03dc1
Igor Usenko <usenkoigor01@gmail.com>
2025-06-15 17:17:36 +0300

    Обновление логики и доступных моделей

--- ansi.py
+++ ansi.py
@@ -1,4 +1,3 @@
-
 import os
 import re
 import sys
@@ -11,27 +10,31 @@ import winreg
 import configparser
 import webbrowser
 import requests
+
 from datetime import datetime
 from colorama import init, Fore
-from webscout import KOBOLDAI, BLACKBOXAI, PhindSearch, WEBS as w
 from packaging import version
 
-
-CURRENT_VERSION = "1.3.3"
-
 init()  # Инициализация colorama
+
+# Функция для удаления эмодзи (Unicode > 0xFFFF)
+def remove_emojis(text):
+    return re.sub(r'[\U00010000-\U0010ffff]', '', text)
+
+CURRENT_VERSION = "1.3.4"
+config_file = "config.ini"
+ansi_folder = "C:\\ansi\\"
+ansi_config_file_path = os.path.join(ansi_folder, config_file)
 
 def update_app(update_url):
    webbrowser.open(update_url)
 
 def check_for_updates():
     try:
-        # Получение информации о последнем релизе на GitHub
         response = requests.get("https://api.github.com/repos/Processori7/ansi/releases/latest")
         response.raise_for_status()
         latest_release = response.json()
 
-        # Получение ссылки на файл exe последней версии
         assets = latest_release["assets"]
         for asset in assets:
             if asset["name"].endswith(".exe"):
@@ -41,71 +44,74 @@ def check_for_updates():
             print("Не удалось найти файл exe для последней версии.")
             return
 
-        # Сравнение текущей версии с последней версией
         latest_version_str = latest_release["tag_name"]
         match = re.search(r'\d+\.\d+', latest_version_str)
-        if match:
-            latest_version = match.group()
-        else:
-            latest_version = latest_version_str
+        latest_version = match.group() if match else latest_version_str
 
         if version.parse(latest_version) > version.parse(CURRENT_VERSION):
-            # Предложение пользователю обновление
-            ans = input(f"Доступна новая версия {latest_version}. Хотите обновить?\nВведите да - для обновления.\n>>>").lower()
+            ans = input(f"Доступна новая версия {latest_version}. Хотите обновить?\nВведите да - для обновления.\n>>> ").lower()
             if ans == 'да':
                 update_app(download_url)
     except requests.exceptions.RequestException as e:
-            print("Ошибка при проверке обновлений:", e)
+        print("Ошибка при проверке обновлений:", e)
 
-# Функция для чтения настроек модели из INI-файла
 async def read_config_from_drive_c():
     config = configparser.ConfigParser()
-    config.read(r'C:\ansi\config.ini')
-    if 'model' in config:
-        return config.get('model', 'name')
-    else:
-        return 'claude-3-haiku'
+    try:
+        config.read(ansi_config_file_path)
+        if 'model' in config and 'name' in config['model']:
+            return config.get('model', 'name').strip()
+        else:
+            return 'o3-mini'
+    except Exception as e:
+        print(f"Ошибка при чтении C:\\ansi\\config.ini: {e}")
+        return 'o3-mini'
 
 async def read_model_config():
     config = configparser.ConfigParser()
-    if os.path.exists(r'C:\ansi\config.ini'):
-        config.read(r'C:\ansi\config.ini')
-    else:
-        config.read('config.ini')
-    if 'model' in config:
-        return config.get('model', 'name')
-    else:
-        return 'claude-3-haiku'
+    try:
+        if os.path.exists(ansi_config_file_path):
+            config.read(ansi_config_file_path)
+        else:
+            config.read('config.ini')
 
-# Функция для записи настроек модели в INI-файл
+        if 'model' in config and 'name' in config['model']:
+            return config.get('model', 'name').strip()
+        else:
+            return 'o3-mini'
+    except Exception as e:
+        print(f"Ошибка при чтении config.ini: {e}")
+        return 'o3-mini'
+
 async def write_model_config(model_name):
     config = configparser.ConfigParser()
     config.add_section('model')
-    config.set('model', 'name', model_name)
+    config.set('model', 'name', str(model_name))
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
 
 async def write_model_config_to_drive_c(model_name):
     config = configparser.ConfigParser()
     config.add_section('model')
-    config.set('model', 'name', model_name)
-    with open(r'C:\ansi\config.ini', 'w') as configfile:
+    config.set('model', 'name', str(model_name))
+    with open(ansi_config_file_path, 'w') as configfile:
         config.write(configfile)
 
 async def show_model():
-    await print_flush2("По умолчанию используется: o3-mini.\nДоступные провайдеры и модели:\n1.KoboldAI - any LLM\n2.Blackbox - Blackbox LLM\n3.Phind - Phind LLM\n4.DuckDuck GO Chat models:\n5.o3-mini\n6.gpt-4o-mini\n7.claude-3-haiku\n8.llama-3.1-70b\n9.mixtral-8x7b")
-    ans = input("Введите номер выбранной модели, например 5: ")
-    if ans.isdigit() and int(ans) < 7:
-        model_name = ['KoboldAI', 'Blackbox', 'ThinkAnyAI', 'Phind','o3-mini', 'gpt-4o-mini','claude-3-haiku','llama-3.1-70b','mixtral-8x7b'][int(ans) -1]
-        if os.path.exists(r'C:\ansi\config.ini'):
-            await write_model_config_to_drive_c(model_name)
+    use_model = await read_config_from_drive_c() if os.path.exists(ansi_config_file_path) else await read_model_config()
+    print(f"Сейчас используется: {use_model}.\nДоступные модели:")
+    model_names = await get_Polinations_chat_models()
+    ans = input("Введите название выбранной модели: ").strip()
+    if ans in model_names:
+        if os.path.exists(ansi_config_file_path):
+            await write_model_config_to_drive_c(ans)
         else:
-            await write_model_config(model_name)
+            await write_model_config(ans)
         await print_flush2("Готово, модель добавлена в Config.ini\n")
         await main()
-        return model_name
+        return ans
     else:
-        print("Ошибка! Пожалуйста, введите правильное число.")
+        print("Ошибка! Пожалуйста, введите правильное имя модели.")
         await show_model()
 
 async def add_to_path(path, root=winreg.HKEY_CURRENT_USER, key_path='Environment', access=winreg.KEY_ALL_ACCESS):
@@ -118,31 +124,31 @@ async def add_to_path(path, root=winreg.HKEY_CURRENT_USER, key_path='Environment
     winreg.CloseKey(root_key)
 
 async def clear_terminal():
-    """Очищает терминал."""
     try:
         subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
     except Exception as e:
         print(f"Ошибка при очистке терминала: {e}")
 
 async def print_flush2(text):
-    for char in text:
+    cleaned_text = remove_emojis(text)
+    for char in cleaned_text:
         sys.stdout.write(char)
         sys.stdout.flush()
-    time.sleep(0.005)  # Задержка перед очисткой терминала
+    time.sleep(0.005)
 
 async def print_flush3(text):
-    for char in text:
+    cleaned_text = remove_emojis(text)
+    for char in cleaned_text:
         sys.stdout.write(char)
         sys.stdout.flush()
     print()
 
 async def print_history():
-    ansi_folder = "C:\\ansi\\"
     history_file = os.path.join(ansi_folder, "history.txt")
     if os.path.exists(history_file):
         with open(history_file, "r") as f:
             for line in f:
-                print(line.strip())
+                print(remove_emojis(line.strip()))
         ans = input("\nДля возврата в меню введите 'да' или 'yes'\nДля очистки истории введите cls: ")
         if ans.lower() in ['да', 'yes']:
             await main()
@@ -153,82 +159,67 @@ async def print_history():
     else:
         print("Ой! История не найдена!\n")
 
-async def communicate_with_model(message, model):
-    """Взаимодействует с моделью для генерации ответа."""
-    try:
-        ans = w().chat(message, model=model)  # gpt-4o-mini, mixtral-8x7b, llama-3-70b, claude-3-haiku
-        return ans
-    except Exception as e:
-        return f"Ошибка при общении с моделью: {e}"
-
-async def communicate_with_KoboldAI(user_input):
-    try:
-        koboldai = KOBOLDAI()
-        response = koboldai.chat(user_input)
-        return response
-    except Exception as e:
-        return f"Ошибка при общении с KoboldAI: {e}"
-
-async def communicate_with_BlackboxAI(user_input):
-    try:
-        ai = BLACKBOXAI(
-            is_conversation=True,
-            max_tokens=800,
-            timeout=30,
-            intro=None,
-            filepath=None,
-            update_file=True,
-            proxies={},
-            history_offset=10250,
-            act=None,
-            model=None
-        )
-
-        responce = ai.chat(user_input)
-        return responce
-    except Exception as e:
-        return f"Ошибка при общении с BLACKBOXAI: {e}"
-
-async def communicate_with_Phind(user_input):
-    try:
-        ph = PhindSearch()
-        response = ph.chat(user_input)
-        return response
-    except Exception as e:
-        return f"Ошибка при общении с PhindAI: {e}"
-
 async def save_histoy(user_input, response):
-    ansi_folder = "C:\\ansi\\"
     history_file = os.path.join(ansi_folder, "history.txt")
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    # Сохранение истории в файл
     if not os.path.exists(ansi_folder):
         os.makedirs(ansi_folder)
-    with open(history_file, "a") as f:
+    with open(history_file, "a", encoding="utf-8") as f:
         f.write(f"Дата и время: {timestamp}\nВопрос пользователя: {user_input}\nОтвет Ansi: {response}\n\n")
+
+async def communicate_with_Pollinations_chat(user_input, model):
+    try:
+        url = f"https://text.pollinations.ai/'{user_input}'?model={model}"
+        resp = requests.get(url)
+
+        if resp.ok:
+            # Пытаемся распарсить JSON
+            try:
+                data = resp.json()  # Может быть JSON
+                # Если это JSON и есть 'reasoning_content', возвращаем его без эмодзи
+                return remove_emojis(data.get('reasoning_content', str(data)))  # fallback: выводим JSON как строку
+            except requests.exceptions.JSONDecodeError:
+                # Если не JSON, возвращаем обычный текст
+                return remove_emojis(resp.text)
+        else:
+            return f"Ошибка сервера: {resp.status_code}"
+    except Exception as e:
+        return str(e)
+
+async def get_Polinations_chat_models():
+    names = []
+    try:
+        url = "https://text.pollinations.ai/models"
+        resp = requests.get(url)
+        if resp.ok:
+            models = resp.json()
+            for model in models:
+                if isinstance(model, dict) and "name" in model:
+                    model_name = model["name"]
+                    model_description = model.get("description", "Без описания")
+                    print(f"Название модели: {model_name} Описание: {model_description}")
+                    names.append(str(model_name))
+            return names
+        else:
+            print(f"Ошибка получения списка моделей: {resp.status_code}")
+            return ["o3-mini"]
+    except Exception as e:
+        print(f"Ошибка при получении списка моделей: {e}")
+        return ["o3-mini"]
 
 async def main():
     """Основная функция программы."""
     try:
-        config_file = "config.ini"
-        ansi_folder = "C:\\ansi\\"
-        # Проверяем, запущен ли скрипт от имени администратора
         if ctypes.windll.shell32.IsUserAnAdmin():
-            # Папка для копирования файла
             if not os.path.exists(ansi_folder):
                 os.makedirs(ansi_folder)
                 await add_to_path(ansi_folder)
-
-            # Проверяем наличие файла ansi.exe в папке C:\ansi
             ansi_exe_path = os.path.join(ansi_folder, "ansi.exe")
             if not os.path.exists(ansi_exe_path):
-                # Если файл не найден, копируем его из папки Desktop
                 desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
                 source_path = os.path.join(desktop_path, "ansi.exe")
                 shutil.copy(source_path, ansi_folder)
 
-        # Проверяем, существует ли файл config.ini в папке C:\ansi
-        ansi_config_file_path = os.path.join(ansi_folder, config_file)
         if os.path.exists(ansi_config_file_path):
             model_name = await read_config_from_drive_c()
         else:
@@ -243,11 +234,11 @@ Ansi GPT3 готова к общению.
 - Введите 'выход' или 'ex' или 'exit', чтобы завершить.
 - Введите 'очистить' или 'cls' или 'clear', чтобы удалить переписку.
 - Введите 'история' или 'hsitory', чтобы вывести истрию переписки на экран.
-- Введите 'model' или 'модель', чтобы выбрать LLM из доступных.\n
+- Введите 'model' или 'модель', чтобы выбрать LLM из доступных.
 """)
 
         while True:
-            user_input = input(Fore.LIGHTGREEN_EX + "Вы: " + Fore.WHITE)
+            user_input = input(Fore.LIGHTGREEN_EX + "\nВы: " + Fore.WHITE)
             print()
             if user_input.lower() in ['история', 'history']:
                 await print_history()
@@ -262,31 +253,15 @@ Ansi GPT3 готова к общению.
                 continue
             else:
                 print(Fore.YELLOW + f"Ansi {model_name} LLM:" + Fore.WHITE, end=' ')
-                if model_name == 'o3-mini' or not os.path.exists('config.ini'):
-                    response = await communicate_with_model(user_input, 'o3-mini')
-                elif model_name == 'claude-3-haiku':
-                    response = await communicate_with_model(user_input, 'claude-3-haiku')
-                elif model_name == 'gpt-4o-mini':
-                    response = await communicate_with_model(user_input, 'gpt-4o-mini')
-                elif model_name == 'llama-3.1-70b':
-                    response = await communicate_with_model(user_input, 'llama-3.1-70b')
-                elif model_name == 'mixtral-8x7b':
-                    response = await communicate_with_model(user_input, 'mixtral-8x7b')
-                elif model_name == 'KoboldAI':
-                    response = await communicate_with_KoboldAI(user_input)
-                elif model_name == 'Blackbox':
-                    response = await communicate_with_BlackboxAI(user_input)
-                elif model_name == 'Phind':
-                    response = await communicate_with_Phind(user_input)
+                if model_name == 'o3-mini' and not os.path.exists('config.ini'):
+                    response = await communicate_with_Pollinations_chat(user_input, 'o3-mini')
                 else:
-                    response = await communicate_with_model(user_input)
-
+                    response = await communicate_with_Pollinations_chat(user_input, model_name)
                 await save_histoy(user_input, response)
                 await print_flush3(response + "\n")
 
     except KeyboardInterrupt:
         print("\nПрограмма завершена пользователем.")
-
     except Exception as e:
         print(f"Внимание! Произошла ошибка: {e}\n")
 
